93d6fad117a043fdfd56f4335d3d9276bdf58dde
C. McEnroe <june@causal.agency>
2019-11-06 15:54:13 -0500

    Change license to GPLv3
    > Notwithstanding any other provision of this License, if you modify the
    > Program, your modified version must prominently offer all users
    > interacting with it remotely through a computer network (if your version
    > supports such interaction) an opportunity to receive the Corresponding
    > Source of your version by providing access to the Corresponding Source
    > from a network server at no charge, through some standard or customary
    > means of facilitating copying of software.
    
    This potentially means that every freenode user, for example, is
    interacting with this software, and offering the corresponding source to
    each of them is an unreasonable burden.

--- state.c
+++ state.c
@@ -1,17 +1,17 @@
 /* Copyright (C) 2019  C. McEnroe <june@causal.agency>
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include <assert.h>
@@ -323,7 +323,7 @@ void stateSync(struct Client *client) {
 	clientFormat(
 		client,
 		":%s NOTICE %s :"
-		"pounce is AGPLv3 fwee softwawe ^w^  code is avaiwable fwom %s\r\n",
+		"pounce is GPLv3 fwee softwawe ^w^  code is avaiwable fwom %s\r\n",
 		Origin, self.nick, SourceURL
 	);
 
