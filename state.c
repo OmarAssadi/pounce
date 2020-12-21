1829dc40c6c3af3768a9feaa99f4aabbb34eca23
C. McEnroe <june@causal.agency>
2020-12-21 15:29:10 -0500

    Send CAP END if CAP LS response is empty
    Or only unsupported caps. Or, as the corresponding commit in catgirl
    says, "if CAP LS doesn't list anything good."

--- state.c
+++ state.c
@@ -90,7 +90,11 @@ static void handleCap(struct Message *msg) {
 
 	if (!strcmp(msg->params[1], "LS") || !strcmp(msg->params[1], "NEW")) {
 		caps &= ~(CapSASL | CapSTS | CapUnsupported);
-		if (caps) serverFormat("CAP REQ :%s\r\n", capList(caps, NULL));
+		if (caps) {
+			serverFormat("CAP REQ :%s\r\n", capList(caps, NULL));
+		} else {
+			if (!(stateCaps & CapSASL)) serverFormat("CAP END\r\n");
+		}
 
 	} else if (!strcmp(msg->params[1], "ACK")) {
 		stateCaps |= caps;
